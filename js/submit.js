b9c816e2a9a8466eaeab4f5a152d969b8aa1994d
Emir Aydin <emraydn12@gmail.com>
2014-02-16 07:25:29 -0500

    log

--- js/submit.js
+++ js/submit.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
         // let's select and cache all the fields
         var $inputs = $form.find("input");
         // serialize the data in the form
-        var serializedData = [];
+        var serializedData = {};
         serializedData.airline = $("#airline").val();
         serializedData.flight_no = $("#flight_no").val();
         serializedData.access_key = $("#access_key").val();
@@ -50,7 +50,7 @@ $(document).ready(function() {
             console.log(textStatus);
             console.log(jqXHR);
             // log a message to the console
-            if (textStatus.success == 'yes') {
+            if (response == 'success') {
                 console.log("Confirmed! Check your Pebble for flight updates!");
                 $('#second').hide();
                 $("#formscenter").append('<p>Confirmed! Check your Pebble for flight updates!</p>');
