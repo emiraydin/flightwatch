4838a997c305de501c4e3a5a87143e9a89438d09
Emir Aydin <emraydn12@gmail.com>
2014-02-16 07:32:43 -0500

    success

--- js/submit.js
+++ js/submit.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
             console.log(textStatus);
             console.log(jqXHR);
             // log a message to the console
-            if (textStatus.success == 'yes') {
+            if (response.success == 'yes') {
                 console.log("Confirmed! Check your Pebble for flight updates!");
                 $('#second').hide();
                 $("#formscenter").append('<p>Confirmed! Check your Pebble for flight updates!</p>');
