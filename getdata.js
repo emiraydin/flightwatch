6ce6b8e1e21a2bc12dce7bf638a492aa0d34e5c7
Emir Aydin <emraydn12@gmail.com>
2014-02-18 04:02:42 -0500

    Use FlightStats API

--- getdata.js
+++ getdata.js
@@ -1,3 +1,4 @@
-ajax({ url: 'http://flightwatch.org/main.php?feature=generate_access_key', method: 'get', type: 'json' }, function(data){
-  simply.title('Welcome! Here is your access key: ' + data.access_key);
+ajax({ url: 'http://flightwatch.dev/main.php?feature=get_flight_from_access_key&access_key=73hp', method: 'get', type: 'json' }, function(data){
+  simply.title(data.airline + ' ' + data.flightNumber + '\n' + data.origin + ' to ' + data.destination + '\n'
+  	+ data.flightStatusType + '\nGATE: ' + data.gate + '\n');
 });
